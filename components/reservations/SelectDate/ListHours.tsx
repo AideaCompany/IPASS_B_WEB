8209275f563fc8b5fc6a69571dc69472f6138aba
Johan <johanavilax@gmail.com>
2022-05-17 12:20:21 -0500

    list hour with cards

--- components/reservations/SelectDate/ListHours.tsx
+++ components/reservations/SelectDate/ListHours.tsx
@@ -2,9 +2,19 @@ import { IService } from '@/types/interfaces/services/Services.interface'
 import { IStaff } from '@/types/interfaces/staff/staff.interface'
 import { IStores } from '@/types/interfaces/Stores/stores.interface'
 import React from 'react'
+import CardHour from './CardHour'
 
 const ListHours = ({ service }: { service: IService; store: IStores; staff?: IStaff }) => {
-  return <div>ListHours</div>
+  const data = [1, 2]
+  return (
+    <div>
+      {data.map((e, i) => (
+        <React.Fragment key={i}>
+          <CardHour />
+        </React.Fragment>
+      ))}
+    </div>
+  )
 }
 
 export default ListHours
