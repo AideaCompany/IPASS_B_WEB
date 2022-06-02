ca6e21e92107b3e065a7cd67e1de58dd11835764
jdiez-bdb <jdiez@bancodebogota.com.co>
2022-06-02 07:46:40 -0500

    hours

--- components/reservations/SelectDate/CardHour.tsx
+++ components/reservations/SelectDate/CardHour.tsx
@@ -1,7 +1,7 @@
 import { availableHours } from '@/types/interfaces/services/Services.interface'
 import React from 'react'
 
-const CardHour = ({ onClick, hour }: { onClick: () => void; hour: availableHours }) => {
+const CardHour = ({ onClick, hour }: { onClick: () => void; hour: availableHours | string }) => {
   //#region functions
 
   const formatHour = (value: string) => {
@@ -16,7 +16,7 @@ const CardHour = ({ onClick, hour }: { onClick: () => void; hour: availableHours
       className="hover:underline  hover:bg-gold hover:text-white font-Butler cursor-pointer border font-medium text-center font-bond text-lg"
       onClick={onClick}
     >
-      {formatHour(hour.hour)}
+      {(hour as availableHours)?.hour ? formatHour((hour as availableHours).hour) : formatHour(hour as string)}
     </p>
   )
 }
