6e1cd199abef3adbc437259475155aeb63ad6644
Johan <johanavilax@gmail.com>
2022-06-05 01:31:06 -0500

    merge

--- components/ModalService.tsx
+++ components/ModalService.tsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import useReservation from '@/providers/ReservationContext'
 import { IService } from '@/types/interfaces/services/Services.interface'
-=======
-import useReservation, { stepsPageReservation } from '@/providers/ReservationContext'
->>>>>>> a19138c (fix compile)
 import React from 'react'
 import Button from './Button'
 
