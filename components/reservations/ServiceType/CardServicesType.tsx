6fcf25fc1b23dab5e22ba437c801a5832ded324f
JohanBarrantes <78429726+JohanBarrantes@users.noreply.github.com>
2022-06-22 19:54:44 -0500

    arrays

--- components/reservations/ServiceType/CardServicesType.tsx
+++ components/reservations/ServiceType/CardServicesType.tsx
@@ -1,23 +1,27 @@
+import useReservation, { stepsPageReservation } from '@/providers/ReservationContext'
 import { IServiceType } from '@/types/interfaces/ServiceType/serviceType.interface'
 import { Popover } from 'antd'
 import React from 'react'
 import ModalServiceType from './ModalServiceType'
 const CardServicesType = ({ service }: { service: IServiceType }) => {
+  const { setSelectedServiceType, setStep } = useReservation()
+  const onClick = (value: IServiceType) => {
+    setSelectedServiceType(value)
+    setStep(stepsPageReservation.services)
+  }
   console.log(service.logo.key)
   return (
-    <Popover content={<ModalServiceType service={service} />} placement="leftBottom" trigger="click">
-      <div className="Container_Ser w-56  h-72 lg:max-w-full lg:flex cursor-pointer ">
-        <div className="Image_containerSec">
-          {/* <div className="Image_background"></div> */}
-          <img src={service.logo.key} className="sec-img"></img>
-        </div>
-        <div className="flex flex-col items-center justify-center text-white p-0n  shadow rounded-lg">
-          <h2 className="mt-4 font-bold text-xl text-white text-right">{service?.name}</h2>
-
-          <p className="text-xs text-gray-500 text-center mt-48">Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
-        </div>
+    <div className="Container_Ser w-56  h-72 lg:max-w-full lg:flex cursor-pointer " onClick={() => onClick(service)}>
+      <div className="Image_containerSec">
+        {/* <div className="Image_background"></div> */}
+        <img src={service.logo.key} className="sec-img"></img>
       </div>
-    </Popover>
+      <div className="flex flex-col items-center justify-center text-white p-0n  shadow rounded-lg">
+        <h2 className="mt-4 font-bold text-xl text-white text-right">{service?.name}</h2>
+
+        <p className="text-xs text-gray-500 text-center mt-48">Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
+      </div>
+    </div>
   )
 }
 
--- components/reservations/Stores/CardStore.tsx
+++ components/reservations/Stores/CardStore.tsx
@@ -4,21 +4,26 @@ import numeral from 'numeral'
 const CardStore = ({ onClick, store }: { store: IStores; onClick: (value: IStores) => void }) => {
   return (
     <>
-      <div className="Container_Sto cursor-pointer " onClick={() => onClick(store)}>
+      <div className="Container_Sto cursor-pointer ">
         <div className="Image_containerS ">
           <img alt="" src="/images/Store.png" className="map1-img" />
         </div>
         <div className="Container_Information">
-          <div className="mb-8 h-24">
+          <div className="mb-8 h-16">
             <p className="text-sm text-gray-600 flex items-center"></p>
 
-            <div className="w-full ">
-              <Rate></Rate>
-              <div className="text-gray-900 font-bold text-sm  mt-2"> {store.name}</div>
-              <div className="text-gray-700 text-xs  mt-2"> Dir:{store.address}</div>
-              <div className="text-gray-700 text-xs  mt-2 "> Tel:{store.phone}</div>
-              <div className="text-gray-700 text-xs  mt-2 "> Contacto: {store.contact}</div>
-              {store.distance && <div className="text-gray-700 text-xs  mt-2 "> {numeral(store.distance).format('00.00')} km de ti</div>}
+            <div className="w-full flex ">
+              <div className="w-1/2">
+                <div className="text-gray-900 font-bold text-sm  mt-2 w-full text-left"> {store.name}</div>
+                <div className="text-gray-700 text-xs  mt-1 w-full text-left"> Contacto: {store.contact}</div>
+                <div className="text-gray-700 text-xs w-1/2 text-left mt-1"> Dir:{store.address}</div>
+                <div className="text-gray-700 text-xs w-1/2 text-left mt-1 "> Tel:{store.phone}</div>
+              </div>
+              <div className="text-gray-700 text-xs  mt-2 w-1/2 text-center">
+                {' '}
+                <Rate style={{ fontSize: '15px' }}></Rate>
+                {store.distance && <div className="text-gray-700 text-xs  text-right mt-2 "> {numeral(store.distance).format('00.00')} km de ti</div>}
+              </div>
             </div>
           </div>
         </div>
