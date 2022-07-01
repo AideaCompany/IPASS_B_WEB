baa2b54a5bc7f67b7672bde16c0af5af7539a016
Johan <johanavilax@gmail.com>
2022-07-01 18:16:47 -0500

    fix text

--- components/reservations/Hair.tsx
+++ components/reservations/Hair.tsx
@@ -11,7 +11,7 @@ const Hair = () => {
   return (
     <div className="Main_Container">
       <div className="Container_bar ">
-        <div className="Search_bar flex appearance-none   left-2 m-5 sm:max-w-screen-sm text-stone-900">
+        {/* <div className="Search_bar flex appearance-none   left-2 m-5 sm:max-w-screen-sm text-stone-900">
           <svg className="w-6 h-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"></path>
           </svg>
@@ -21,10 +21,10 @@ const Hair = () => {
         <div className="Search_list flex appearance-none border-b  left-2 m-5 sm:max-w-screen-sm text-stone-900">
           <input type="text" className="appearance-none bg-transparent px-4 py-2 w-64" placeholder="Seleccionar tipo de servicio"></input>
           <CaretDownOutlined />
-        </div>
-        <div className="Main_tittle ">
+        </div> */}
+        {/* <div className="Main_tittle ">
           <p className="Title font-Gothic text-right "> Servicios</p>{' '}
-        </div>
+        </div> */}
       </div>
       <div className="Container_Resume  w-full h-14 pt-0 px-64 content-center">
         <div className="Search_resume  flex appearance-none border-b w-52 left-2 m-5 content-center sm:max-w-screen-sm text-stone-900">
