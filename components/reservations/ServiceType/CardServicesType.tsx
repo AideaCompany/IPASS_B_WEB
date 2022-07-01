1b5e3c7f06fb0bbc53f35226be0df05e06e18bed
JohanBarrantes <78429726+JohanBarrantes@users.noreply.github.com>
2022-07-01 18:05:50 -0500

    arrays

--- components/reservations/ServiceType/CardServicesType.tsx
+++ components/reservations/ServiceType/CardServicesType.tsx
@@ -14,11 +14,10 @@ const CardServicesType = ({ service }: { service: IServiceType }) => {
     <div className="Container_Ser w-56  h-72 lg:max-w-full lg:flex cursor-pointer " onClick={() => onClick(service)}>
       <div className="Image_containerSec">
         {/* <div className="Image_background"></div> */}
-        <img src={service.logo.key} className="sec-img"></img>
+        <img src={service.logo.key} className="sec-img w-full"></img>
       </div>
-      <div className="flex flex-col items-center justify-center text-white p-0n  shadow rounded-lg">
-        <h2 className="mt-4 font-bold text-xl text-white text-right">{service?.name}</h2>
-
+      <div className="flex flex-col items-center justify-center w-full text-white p-0  shadow rounded-lg">
+        <h2 className="mt-4 font-bold text-xl text-white text-center w-full ">{service?.name}</h2>
         <p className="text-xs text-gray-500 text-center mt-48">Lorem ipsum dolor sit amet, consectetur adipisicing eli</p>
       </div>
     </div>
